50e9c8bb8c54afdc338b0b56ae81fff4286d436d
yankovan <yankovan@vmware.com>
2021-07-09 00:01:17 +0300

    Properties demo

--- src/main/java/com/spring/preparation/Demo.java
+++ src/main/java/com/spring/preparation/Demo.java
@@ -1,16 +1,18 @@
 package com.spring.preparation;
 
+import com.spring.preparation.config.ApplicationProperties;
 import com.spring.preparation.service.EmployeesService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @ComponentScan
 public class Demo {
 
     public void run() {
         System.out.println("Application is running");
-        beanLifecycleDemo();
+        propertySourcesDemo();
     }
 
     private void containerDemo() {
@@ -22,5 +24,13 @@ public class Demo {
     private void beanLifecycleDemo() {
         final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(Demo.class);
         applicationContext.getBean("beanLifecycle");
+    }
+
+    private void propertySourcesDemo() {
+        final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(Demo.class);
+        final ApplicationProperties applicationProperties = (ApplicationProperties) applicationContext.getBean("applicationProperties");
+        final Environment environment = (Environment) applicationContext.getBean("environment");
+        System.out.println("Application name from Environment bean: " + environment.getProperty("application.name"));
+        System.out.println("Application name from properties file: " + applicationProperties.getApplicationName());
     }
 }
